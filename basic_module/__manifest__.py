38ad53613bcde814c346ba60c5d56f5085bc851a
lhalb <l.halbauer@web.de>
2022-08-15 13:23:26 +0200

    Created first minimal model

--- basic_module/__manifest__.py
+++ basic_module/__manifest__.py
@@ -4,5 +4,8 @@
     'version': '0.1',
     'depends': [
         'base'
-    ]
+    ],
+    'installable': True,
+    'application': True,
+    'auto-install': True
 }
---
+++ basic_module/models/real_estate.py
@@ -0,0 +1,7 @@
+from odoo import models
+
+
+class MyModel(models.Model):
+    _name = "test.model"
+    _description = "Real Estate"
+
