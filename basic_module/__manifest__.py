17136ed5c3f67bd753c913e1483801f3e3dfe021
Lars Halbauer <l.halbauer@web.de>
2022-08-16 20:18:26 +0200

    Created first menus

--- basic_module/__manifest__.py
+++ basic_module/__manifest__.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Lars',
-    'version': '0.1',
+    'version': '0.2',
     'depends': [
         'base'
     ],
     'data': [
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menu.xml'
          ],
     'installable': True,
     'application': True,
